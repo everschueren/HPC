e13717fa596121827410991a82b4678632766c18
erik verschueren <erik.verschueren@gmail.com>
2013-07-16 19:00:32 -0700

    updated pipeline to write results in separate dirs + tests added script to generate saint input files and added saint to pipeline

--- MSPipeline/src/APMSPipeline.py
+++ MSPipeline/src/APMSPipeline.py
@@ -44,23 +44,29 @@ def pipeline(config):
 	remove_file = dir+config.get("files","remove")
 	collapse_file = dir+config.get("files","collapse")
 	exclusions_file = dir+config.get("files","exclusions")
-	output_dir = config.get("files","output_dir") + '/'
+	output_dir = dir + config.get("files","output_dir") + '/'
 	
 	##derived
 	bname = os.path.splitext(os.path.basename(data_file))[0]
+	
+	#################################
+	## PREPARE CONTAINERS FOR RESULTS
 
-	## 
-
+	if not os.path.exists(output_dir):
+		os.mkdir(output_dir)
 	mist_metrics_file=""
 	mist_self_score_file=""
 	mist_hiv_score_file=""
 	comppass_score_file=""
 
+
 	## ################## 
 	## 1. merge additions
 
 	tmp_in_file = data_file
+	
 	print(">> MERGING KEYS TO DATA\t\t" + tmp_in_file)
+	bname = bname + "_wKEYS"
 	tmp_out_file = output_dir+bname+".txt"
 	f = open(tmp_out_file,'w')
 	subprocess.call([src_dir+'FileIO/merge_additions.pl', keys_file, tmp_in_file, config.get("general","data_format")], stdout=f)
@@ -69,8 +75,10 @@ def pipeline(config):
 	## ##########################
 	## 1.B merge with mastertable
 
+	tmp_in_file = tmp_out_file
+
 	if config.getboolean('general','master_include'):
-		tmp_in_file = tmp_out_file
+		
 		master_file = config.get('general','master_file')
 		
 		print(">> APPENDING DATA FILE TO MASTERFILE\t\t"+master_file)
@@ -96,92 +104,96 @@ def pipeline(config):
 	## ################### 
 	## 2. remove carryover
 
+	tmp_in_file = tmp_out_file
+
 	if config.get("general","remove_carryover"):
-		tmp_in_file = tmp_out_file
+		
 		print(">> REMOVING CARRYOVER FROM\t\t" + tmp_in_file)
 		tmp_out_file = output_dir+bname+'_NoC'+".txt"
 		f = open(tmp_out_file,'w')
 		subprocess.call([src_dir+'FileIO/carryover_removal_comprehensive.pl', tmp_in_file, config.get("general","data_format")], stdout=f)
 		f.close()
+		## SET THESE UP TO BE (OPTIONALLY) USED BY SAINT LATER
+		prospector_noc = tmp_out_file
+	else: 
+		prospector_noc = tmp_in_file
 
 	## ####################### 
 	## 3. converting to matrix
-	# $data = $ARGV[0];
-	# $format = $ARGV[1];
-	# $inputdir = $ARGV[2];
-	# $remove = $ARGV[3];
-	# $collapse = $ARGV[4];
-	# $exclusions = $ARGV[5];
 
 	tmp_in_file = tmp_out_file
+
 	print(">> CONVERTING TO MATRIX\t\t" + tmp_in_file)
 	tmp_out_file = output_dir+bname+'_MAT'+".txt"
 	f = open(tmp_out_file,'w')
 	subprocess.call([src_dir+'FileIO/convert_to_matrix_format_comprehensive.pl', tmp_in_file, config.get("general","data_format"), remove_file, collapse_file, exclusions_file], stdout=f)
 	f.close()
-
-	## ########################## 
-	## 4. checking file integrity
-
 	tmp_in_file = tmp_out_file
 	print(">> CHECKING MATRIX INTEGRITY\t\t" + tmp_in_file)
 	subprocess.call([src_dir+'FileIO/FileCheck.py', tmp_in_file])
 
-	##################################
-	## 5. MiST scoring with HIV params
+	###################################
+	## 4. MiST scoring with HIV/SELF params
+	
+	tmp_in_file = tmp_out_file ## MATRIX FORMAT 
+
+	mist_dir = output_dir+"/MIST/"
+	if not os.path.exists(mist_dir):
+		os.mkdir(mist_dir)
 
 	if config.getboolean('mist','enabled'):
 		print(">> SCORING MiST [HIV PARAMS]\t\t" + tmp_in_file)
-		tmp_out_file = output_dir+bname+'_MIST_HIV'+".txt"
+		tmp_out_file = mist_dir+bname+'_MIST_HIV'+".txt"
 		subprocess.call([src_dir+'MiST/MiST.py', tmp_in_file, tmp_out_file, config.get('mist','filter'), config.get('mist','training')]) 
-		mist_hiv_score_file = output_dir+bname+'_MIST_HIV'+"_scores.txt"
-
-	###################################
-	## 6. MiST scoring with SELF params
+		mist_hiv_score_file = mist_dir+bname+'_MIST_HIV'+"_scores.txt"
 
 	if config.getboolean('mist_self','enabled'):
 		print(">> SCORING MiST [SELF PARAMS]\t\t" + tmp_in_file)
-		tmp_out_file = output_dir+bname+'_MIST_SELF'+".txt"
+		tmp_out_file = mist_dir+bname+'_MIST_SELF'+".txt"
 		subprocess.call([src_dir+'MiST/MiST.py', tmp_in_file, tmp_out_file, config.get('mist_self','filter'), config.get('mist_self','training')]) 
-		mist_self_score_file = output_dir+bname+'_MIST_SELF'+"_scores.txt"
-		mist_metrics_file = output_dir+bname+'_MIST_SELF'+"_metrics.txt"
+		mist_self_score_file = mist_dir+bname+'_MIST_SELF'+"_scores.txt"
+		mist_metrics_file = mist_dir+bname+'_MIST_SELF'+"_metrics.txt"
 
-	######################
-	## 7. COMPPASS scoring 
+	#######################
+	## 5. COMPPASS scoring 
 
-	## replaced by newer version
-	# if config.getboolean('comppass','enabled'):
-	# 	print(">> SCORING COMPPASS\t\t" + tmp_in_file)
-	# 	tmp_out_file = output_dir+bname+'_COMPPASS'+".txt"
-	# 	f = open(tmp_out_file,'w')
-	# 	subprocess.call(['Comppass/GetComppass.py', tmp_in_file], stdout=f)
-	# 	f.close()
+	comppass_dir = output_dir+"/COMPPASS/"
+	if not os.path.exists(comppass_dir):
+		os.mkdir(comppass_dir)
 
 	if config.getboolean('comppass','enabled'):
 		print(">> SCORING COMPPASS\t\t" + tmp_in_file)
-		tmp_out_file = output_dir+bname+'_COMPPASS'+".txt"
+		tmp_out_file = comppass_dir+bname+'_COMPPASS'+".txt"
 		subprocess.call([src_dir+'Comppass/Comppass.R', '-d', tmp_in_file, '-o', tmp_out_file])
 		comppass_score_file = tmp_out_file
 
 	###################
 	## 7. SAiNT scoring
 
+	saint_dir = output_dir+"SAINT/"
+	if not os.path.exists(saint_dir):
+		os.mkdir(saint_dir)
+
 	if config.getboolean('saint','enabled'):
-		print(">> SCORING SAINT\t\t" + tmp_in_file)
-		tmp_out_file = output_dir+bname+'_SAINT'+".txt"
-		subprocess.call([bin_dir+'saint/saint-spc-noctrl-matrix', tmp_in_file, tmp_out_file, config.get('saint','nburnin'), config.get('saint','niter'), config.get('saint','ff')])
+		os.chdir(saint_dir)	
+		print(">> SCORING SAINT\t\t" + prospector_noc)
+		control_reduction = config.get("saint", "control_reduction")
+		burnin = config.get("saint", "burnin")
+		iter = config.get("saint", "iter")
+		lowmode = config.get("saint", "lowmode")
+		minfold = config.get("saint", "minfold")
+		normalize = config.get("saint", "normalize")
+
+		subprocess.call([src_dir+"Saint/SaintInput.py", '--dir', saint_dir, '--prospector_file', prospector_noc, '--collapse_file', collapse_file, '--remove_file', remove_file])
+		subprocess.call([bin_dir+"saint/saint-reformat", saint_dir+"saint_interactions.txt", saint_dir+"saint_preys.txt", saint_dir+"saint_baits.txt", control_reduction])
+		subprocess.call([bin_dir+'saint/saint-spc-ctrl', saint_dir+"interaction.new", saint_dir+"prey.new", saint_dir+"bait.new", burnin, iter, lowmode, minfold, normalize])		
+
 
 	####################
 	## 8. COLLECT SCORES
 
 	print(">> COLLECT SCORES AND ANNOTATE\t\t")
 	tmp_out_file = output_dir+bname+'_ALLSCORES'+".txt"
-
-	## replaced by newer version
-	# f = open(tmp_out_file,'w')
-	# file_dir = config.get("collect","file_dir")
-	# subprocess.call([src_dir+'FileIO/convert_out_to_final_comprehensive.pl', output_dir+bname, config.get("general","data_format"), collapse_file, file_dir + config.get("collect","gene_names"), file_dir + config.get("collect","uniprot_entrez")], stdout=f )
-	# f.close()
 
 	uniprot_dir = config.get("collect", "uniprot_dir")
 	species = config.get("collect", "species") 
@@ -228,7 +240,8 @@ def main(argv=None):
 				alone = value
 
 		## first read config from file 
-		cfg = readConfig(config)
+		config = "/Users/everschueren/Projects/HPCKrogan/Scripts/MSPipeline/tests/hcv/TestConfig.txt"
+		cfg = readConfig(config) 
 		## then overwrite with command line args
 		## to implement
 
---
+++ MSPipeline/src/Saint/SaintInput.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+SaintWrapper.py
+
+Created by erik verschueren on 2013-07-15.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import os
+import sys
+import getopt
+import subprocess
+import csv
+
+help_message = '''
+The help message goes here.
+'''
+
+def skip_comments(iterable):
+    for line in iterable:
+        if not line.startswith('#'):
+            yield line
+
+def collapseFileTodict(collapse_file):
+	d = {}
+	with open(collapse_file,"r") as cf:
+		cf_reader = csv.reader(cf, delimiter="\t")
+		for line in cf_reader:
+			key = line[0]
+			val = line[1]
+			d[key] = val
+	return d
+
+def removeFileTodict(remove_file):
+	d = {}
+	with open(remove_file,"r") as rf:
+		rf_reader = csv.reader(skip_comments(rf), delimiter="\t")
+		for line in rf_reader:
+			key = line[0]
+			d[key] = True
+	return d
+
+def prospectorToSaintFormat(prospector_file, dir, collapse_file, remove_file):
+
+	interactions_file = dir + "/" + "saint_interactions.txt"
+	bait_file = dir + "/" + "saint_baits.txt"
+	prey_file = dir + "/" + "saint_preys.txt"
+
+	collapse_dict = collapseFileTodict(collapse_file)
+	remove_dict = removeFileTodict(remove_file)
+
+	## write the interactions file
+	with open(prospector_file,"r") as csvfile:
+		reader = csv.reader(csvfile, delimiter="\t")
+		reader.next()
+		reader.next()
+		with open(interactions_file,"w") as interactions_out, open(bait_file,"w") as bait_out, open(prey_file,"w") as prey_out:
+			
+			prev_ip = ""
+			prey_set = set()
+
+			for line in reader:
+				ip = line[1]
+				
+				## check if the IP is in the 'remove'  list
+				if ip not in remove_dict.keys():
+					bait = line[0]
+					if bait in collapse_dict.keys():
+						bait = collapse_dict[bait] ## give the bait the name from the collapse list
+					prey = line[4]
+					tsc = line[5]
+					mw = line[9]
+
+					## 1. write out ALL interactions
+					interactions_out.write(ip+"\t"+bait+"\t"+prey+"\t"+tsc+"\n")
+
+					## 2. for the bait file only write out info if we're dealing with a new IP
+					## assuming all IPs are SORTED in the prospectorfile
+					if ip != prev_ip:	
+						if(bait == "negative" or bait == "control" or bait == "none"):
+							control = "C" ## CONTROL
+						else:
+							control = "T" ## TEST
+						bait_out.write((ip+"\t"+bait+"\t"+control+"\n"))
+						prev_ip = ip
+
+					## 3. write out prey info if it wasn't observed before
+					if prey not in prey_set:
+						prey_out.write(prey+"\t"+mw+"\t"+prey+"\n")
+						prey_set.add(prey)
+			
+			interactions_out.close()
+			bait_out.close()
+			prey_out.close()
+
+
+class Usage(Exception):
+	def __init__(self, msg):
+		self.msg = msg		
+
+
+def main(argv=None):
+
+	dir = os.getcwd()
+
+	## DEFAULTS ########
+	prospector_file = ""
+	collapse_file = ""
+	remove_file= ""
+	
+
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(argv[1:], "ho:dpcr", ["help", "output=", "dir=", "prospector_file=", "collapse_file=", "remove_file="])
+		except getopt.error, msg:
+			raise Usage(msg)
+	
+		# option processing
+		for option, value in opts:
+			if option == "-v":
+				verbose = True
+			if option in ("-h", "--help"):
+				raise Usage(help_message)
+			if option in ("-d", "--dir"):
+				dir = value
+			if option in ("-p", "--prospector_file"):
+				prospector_file = value
+			if option in ("-c", "--collapse_file"):
+				collapse_file = value
+			if option in ("-r", "--remove_file"):
+				remove_file = value
+
+		print(collapse_file)
+		prospectorToSaintFormat(prospector_file, dir, collapse_file, remove_file)
+
+	except Usage, err:
+		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+		print >> sys.stderr, "\t for help use --help"
+		return 2
+
+
+if __name__ == "__main__":
+		sys.exit(main())
