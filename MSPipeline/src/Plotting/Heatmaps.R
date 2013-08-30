6596b0b0a8c143cc9f03d3b5e324187c9c1b3fe2
erik verschueren <erik.verschueren@gmail.com>
2013-08-30 10:01:16 -0700

    Heatmap legend font size and position, still needs to be moved away from a hardcode solution

--- MSPipeline/src/Plotting/Heatmaps.R
+++ MSPipeline/src/Plotting/Heatmaps.R
@@ -20,8 +20,8 @@ heatmap.EV = function(data, cexRow=1.0, palette.breaks=NULL, color.panel=NULL){
   heatmap.2(data, dendrogram="row",key=F,trace="none",col=color.panel,lmat=lmat, lhei=lhei, lwid=lwid, margins=margins, cexRow=cexRow, breaks=palette.breaks, cexCol=cexRow)
   
   ## print legend
-  leg_x_offset = 0.90
+  leg_x_offset = 0.95
   leg_y_offset = 0.01
   legend_coords = cbind(x = c(0,0.015,0.015,0) + leg_x_offset, y =c(0.1,0.1,0,0)  + leg_y_offset)
-  legend.gradient(legend_coords,cols=color.panel, limits=c(round(min(palette.breaks),2),round(max(palette.breaks),2)), title="",cex=cexRow-1)
+  legend.gradient(legend_coords,cols=color.panel, limits=c(round(min(palette.breaks),2),round(max(palette.breaks),2)), title="",cex=cexRow)
 }
