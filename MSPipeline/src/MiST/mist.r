dd2826446e3583525c5ba01731e89f82bc3479cf
jvondollen <jvondollen@gmail.com>
2013-12-30 11:43:04 -0800

    added "Reproducibility" and "Abundance" calculations

--- MSPipeline/src/MiST/mist.r
+++ MSPipeline/src/MiST/mist.r
@@ -3,8 +3,9 @@
 
 
 
-
+# simplify/convert data into workable form
 processMatrix <- function(x){
+	baits <- x[1,5:dim(x)[2]]
 	x <- x[-c(1:2),]
 	names(x)[1:3] <- c("Preys", "PepAtlas", "Length")
 	idx <- c(2,3,5:dim(x)[2])
@@ -12,19 +13,51 @@ processMatrix <- function(x){
 	for(i in idx){
 		x[,i] <- as.numeric(x[,i])
 	}
-	return(x)
+	return(list(x, baits))
 }
 
 
 
-getM3D <- function(x){
+getM3D_normalized <- function(x){
 	x1 <- x[,5:dim(x)[2]]/x[,3]
-	# normalize by column (of x)
+	# normalize by column (of x)	-	M3D
 	x1 <- scale(x1, center=FALSE, scale=colSums(x[,5:dim(x)[2]]))
-	# normalize by column (of x1)	
+	# normalize by column (of x1)	-	M3D normalized
 	x1 <- scale(x1, center=FALSE, scale=colSums(x1))	
-	# normalize by row
-	temp <- t(scale(t(x1), center=FALSE, scale=colSums(t(x1))))
+	return(x1)
+}
+
+# calculate Abundance, Reproducibility, and Specificity
+getMetrics <- function(x, baits){
+	reproducibility <- c()
+	abundance <- c()
+	
+	# look at data per bait
+	for( i in unique(baits)){
+		bidx <- which(baits == i)
+		y <- x[,bidx]
+		# normalize by row by bait
+		y <- y/apply(y, 1, sum)
+		y[is.na(y)] <- 0
+		
+		# Reproducibility ("entropies")
+		# -----------------------------
+		y[y!=1 & y>0] = y[y!=1 & y>0] * log2(y[y!=1 & y>0])
+		y[y==1] = (1-1e-10)*log2(1-1e-10)
+		y <- rowSums(y)
+		if(length(bidx)!=1){
+			y = y/log2(1/length(bidx))
+		}else{
+			y = y*0+1
+		}
+		reproducibility <- cbind(reproducibility, y)
+		colnames(reproducibility)[dim(reproducibility)[2]] <- i
+		
+		# Abundance ("averages")
+		# ----------------------
+		abundance <- cbind(abundance, rowSums(x[,bidx])/length(bidx))
+		colnames(abundance)[dim(abundance)[2]] <- i
+	}
 
 }
 
@@ -39,17 +72,20 @@ getM3D <- function(x){
 
 
 
-
-
+##############################################################################################################
 #dat <- read.csv("~/HPC/MSPipeline/tests/3A/input/3A-results.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
 #keys <- read.csv("~/HPC/MSPipeline/tests/3A/input/3A_keys.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
 
-dat <- read.csv("~/HPC/MSPipeline/tests/3A/processed/3A-results_wKEYS_NoC_MAT.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+dat <- read.csv("~/HPC/MSPipeline/tests/3A_IP/processed/3A_IP-results_wKEYS_NoC_MAT.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
 
 dat <- processMatrix(dat)
-x <- dat
+m3d_norm <- getM3D_normalized(dat[[1]])
 
-m3d <- getM3D(dat)
+#################
+x <- m3d_norm
+baits <- unlist(dat[[2]])
+
+x <- getMetrics(m3d_norm, unlist(dat[[2]]))
 
 
 
