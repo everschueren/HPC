ce18844334050a2616e3bed800a82170543b27f3
jvondollen <jvondollen@gmail.com>
2014-03-13 14:03:49 -0700

    Added support for specificity exclusions. Also includes scores based on hiv weights.

--- MSPipeline/src/MiST/mist.r
+++ MSPipeline/src/MiST/mist.r
@@ -26,7 +26,7 @@ getM3D_normalized <- function(x){
 }
 
 # calculate Abundance, Reproducibility, and Specificity
-getMetrics <- function(x, baits){
+getMetrics <- function(x, baits, exclusions){
 	reproducibility <- c()
 	abundance <- c()
 	
@@ -35,15 +35,25 @@ getMetrics <- function(x, baits){
 		bidx <- which(baits == i)
 		y <- x[,bidx]
 		# normalize by row by bait
-		y <- y/apply(y, 1, sum)
+    if(length(bidx)<2){ #if there's only one row for that bait
+		  y <- y/sum(y)
+    }else{
+      y <- y/apply(y, 1, sum)
+    }
 		y[is.na(y)] <- 0
 		
 		# Reproducibility ("entropies")
 		# -----------------------------
 		y[y!=1 & y>0] = y[y!=1 & y>0] * log2(y[y!=1 & y>0])
 		y[y==1] = (1-1e-10)*log2(1-1e-10)
-		y <- rowSums(y)
-		if(length(bidx)!=1){
+		
+		if(length(bidx)<2){ #if there's only one row for that bait
+      y <- sum(y)
+		}else{
+      y <- rowSums(y)
+		}
+    
+    if(length(bidx)!=1){
 			y = y/log2(1/length(bidx))
 		}else{
 			y = y*0+1
@@ -53,15 +63,46 @@ getMetrics <- function(x, baits){
 		
 		# Abundance ("averages")
 		# ----------------------
-		abundance <- cbind(abundance, rowSums(x[,bidx])/length(bidx))
+    if(length(bidx)>1){
+  		abundance <- cbind(abundance, rowSums(x[,bidx])/length(bidx))
+    }else{ #if there's only one row for that bait
+      abundance <- cbind(abundance, x[,bidx]/length(bidx))
+    }
 		colnames(abundance)[dim(abundance)[2]] <- i
 	}
 	
 	# Specificity
 	# ----------------------
-	specificity <- t(apply(abundance,1,function(z) z/sum(z)))
-	
+  # Must account for specificity exclusions
+	specificity <- getSpecificity(abundance, exclusions)
+	#specificity <- t(apply(abundance,1,function(z) z/sum(z)))
 	return(list(reproducibility, abundance, specificity))
+}
+
+
+
+
+
+
+getSpecificity <- function(abundance, exclusions){
+  specificity <- abundance
+  for( i in 1:dim(abundance)[2]){
+
+    
+    bait <- colnames(abundance)[i]
+    # find bait in exclusions list
+    idx <- which(exclusions[,1]==bait)
+    exes <- unique(unlist(strsplit(exclusions[idx,2],"\\|")))
+    # find where excluded baits are in matrix
+    idx <- which(!is.na(match(colnames(abundance), exes)))
+    
+    if(rowSums(abundance[,-idx])>0){
+      specificity[,i] <- specificity[,i]/rowSums(abundance[,-idx])
+    }else{
+      specificity[,i] <- 0
+    }
+  }
+  return(specificity)
 }
 
 # vectorize the metrics while keeping the names straight
@@ -91,24 +132,22 @@ doPCA <- function(R,A,S){
 	return(scores)
 }
 
-
+#scores <- cbind(scores, mist_hiv=scores$Repro*0.30853 + scores$Abundance*0.00596 + scores$Specificity*0.68551 )
 ##############################################################################################################
-#dat <- read.csv("~/HPC/MSPipeline/tests/3A/input/3A-results.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
-#keys <- read.csv("~/HPC/MSPipeline/tests/3A/input/3A_keys.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
 
-#dat <- read.csv("~/HPC/MSPipeline/tests/3A_IP/processed/3A_IP-results_wKEYS_NoC_MAT.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
-dat <- read.csv("~/HPC/MSPipeline/tests/HHV8_glaunsinger/BJAB/data/processed/BJAB_data_wKEYS_NoC_MAT.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
-
+dat <- read.delim("~/HPC/MSPipeline/tests/Black/celona/data/processed/celona_data_wKEYS_NoC_MAT.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
+exclusions <- read.delim("~/HPC/MSPipeline/tests/Black/celona/data/input/celona_exclusions.txt", sep="\t", header=FALSE, stringsAsFactors=FALSE)
 
 dat <- processMatrix(dat)
 m3d_norm <- getM3D_normalized(dat[[1]])
 
-dat <- getMetrics(m3d_norm, unlist(dat[[2]]))
+dat <- getMetrics(m3d_norm, unlist(dat[[2]]), exclusions)
 R <- vectorize(dat[[1]])
 A <- vectorize(dat[[2]])
 S <- vectorize(dat[[3]])
 
-x <- doPCA(R,A,S)
+x <- doPCA(R,A,S) #This is more or less ignored since we always use hiv weights
+x <- cbind(x, mist_hiv_score=x$Repro*0.30853 + x$Abundance*0.00596 + x$Specificity*0.68551 )
 
 write.table(x, "~/HPC/MSPipeline/tests/HHV8_glaunsinger/BJAB/data/processed/MiST_scores_1.txt", row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t" )
 
